4f9d004bc28acb2490d7183b6674b0521f141342
nana <comfortappiah.ca@gmail.com>
2024-04-06 19:09:00 +0000

    adding conditions

--- game.py
+++ game.py
@@ -3,5 +3,13 @@ print(welcome)
 name= input("what is you name? ")
 age =input("how are old are you ? ")
 print("hello" , name , "you are " , age, "years old")
-isOlder = int(age)>=18
-print(isOlder)
+# isOlder = int(age)>=18
+# print(isOlder)
+if age>18:
+   print("you are old enough to play!") 
+   want_to_play=input("Do you want to pay? ")
+   if want_to_play=="yes":
+      print("Let's play!")
+elif: 
+  print("you are not old enogh to play...")    
+  
