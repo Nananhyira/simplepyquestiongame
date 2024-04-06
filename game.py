dfe1d81a22a001c300c477313b775d4c22690c0e
nana <comfortappiah.ca@gmail.com>
2024-04-06 18:04:16 +0000

    creating variables and inputs

--- game.py
+++ game.py
@@ -2,4 +2,4 @@ welcome ="WELCOME TO MY SIMPLE GAME!"
 print(welcome)
 name= input("what is you name? ")
 age =input("how are old are you ? ")
-# print("hello" , name , "you are " , age, "years old")
+print("hello" , name , "you are " , age, "years old")
